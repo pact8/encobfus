6903d39e1f76a49c3b794c84624f4b372a803e27
pact8 <71283232+pact8@users.noreply.github.com>
2023-09-13 21:19:32 -0300

    improving the code
    I added the functionality of a random variable and module name, which makes it almost impossible to read the obfuscated code

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 from colorama import init, Fore, Back, Style
 import argparse
-parser = argparse.ArgumentParser(description='encryption obfuscator')
-parser.add_argument('input', type=str, help='path to the input script')
-parser.add_argument('output', type=str, help='path to the output script')
-parser.add_argument('encmethod', type=str, help='encryption method')
-args = parser.parse_args()
-
+import random
+import string
 banner = """                                 d8b         ,d8888b                 
                                  ?88         88P'                    
                                   88b     d888888P                   
@@ -16,12 +12,30 @@ d8b_,dP  88P' ?8bd8P' `Pd8P' ?88  88P `?8b  88P     d88   88  ?8b,
                                                                      
                                                                      
                                                                      """
-
 init()
-
-
 print(Fore.CYAN + banner + Style.RESET_ALL)
+encmtds = ["FERNET"]
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--list', action='store_true', help='list the available encmethods')
+parser.add_argument('input', type=str, nargs='?', default=None, help='path to the input script')
+parser.add_argument('output', type=str, nargs='?', default=None, help='path to the output script')
+parser.add_argument('encmethod', type=str, nargs='?', default=None, help='encryption method')
+args = parser.parse_args()
+if args.list:
+    print(Fore.YELLOW + "Encryption Methods:")
+    for method in encmtds:
+        print(Fore.WHITE + Back.RED + method + Style.RESET_ALL)
+    print(Style.RESET_ALL)
+    exit()
+else:
+	if None in [args.input, args.output, args.encmethod]:
+		print("error 0x01: i need the other arguments!")
+#real code
+def randomstr(l):
+	characters = string.ascii_letters 
+	s = ''.join(random.choice(characters) for i in range(l))
+	return s
 if args.encmethod == "fernet":
   #importing the fernet libs(require cryptography)
 	from cryptography.fernet import Fernet
@@ -38,17 +52,23 @@ if args.encmethod == "fernet":
 	cf_encrypted = ct.encrypt(content_file)
 	cfe_str = cf_encrypted.decode('utf-8')
 	print(Fore.GREEN + "[*] Content Encrypted" + Style.RESET_ALL)
+	#5
+	ras = randomstr(32)
+	ra = randomstr(32)
+	rb = randomstr(32)
+	rc = randomstr(32)
+	rd = randomstr(32)
+	re = randomstr(32)
 	with open(args.output, "w") as f:
 		f.write(f"""
-from cryptography.fernet import Fernet
-key = {k}
-ciphert = Fernet(key)
-code = "{cfe_str}"
-dec_code = ciphert.decrypt(code)
-dec_code_s = dec_code.decode('utf-8')
-exec(dec_code_s) 
+from cryptography.fernet import Fernet as {ras}
+{ra} = {k}
+{rb} = {ras}({ra})
+{rc} = "{cfe_str}"
+{rd} = {rb}.decrypt({rc})
+{re} = {rd}.decode('utf-8')
+exec({re}) 
 """)
 		f.close()
 
 	print(Fore.GREEN + "[*] Encrypted Content Writed" + Style.RESET_ALL)
-	
